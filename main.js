7972d9497605e29a723e7421b40375e98d88612c
Sounthida Kong <70409867+Sounthi@users.noreply.github.com>
2021-11-01 12:38:47 -0400

    vhsngfesss

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 import './style.scss'
 
+var hamIcon = document.querySelector('.menu__icon');
+
+
+hamIcon.addEventListener('click', function() {
+
+    var hamOptions = document.querySelector('.menu__options');
+    var hamOptionsDisplay = hamOptions.style.display;
+    var menu = document.querySelector('.menu')
+    
+    if (hamOptionsDisplay !== 'flex') {
+
+        hamOptions.style.display = 'flex';
+        hamOptions.style.height = '100vh';
+        menu.style.backgroundColor = 'rgba(244,195,194, 0.8)';
+      }
+    
+      else {
+    
+        hamOptions.style.display = 'none';
+        hamOptions.style.height = '';
+      }
+}) 
